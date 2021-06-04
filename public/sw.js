d307f5072031f621faacb4f4b05f1f2a975e63fc
ags1773 <amogh@quintype.com>
2021-06-04 22:25:58 +0530

    sw basics

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const cacheName = "cache-v1";
+const resourcesToPreCache = [
+  "/",
+  "index.html",
+  "favicon.ico",
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+];
+
+// build cache on install
+self.addEventListener("install", (event) => {
+  console.log("install event occured");
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(resourcesToPreCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("activate event occured");
+});
+
+// fired when network call is made. Used to serve data from cache or if not in cache make network call
+self.addEventListener("fetch", (event) => {
+  console.log(`fetch intercepted for: ${event.request.url}`);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((cachedResponse) => cachedResponse || fetch(event.request))
+  );
+});
